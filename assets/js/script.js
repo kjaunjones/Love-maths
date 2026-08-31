891f26c3b4774d3cc3a67836a4b1c9cddcd0b70f
kjaunjones <kjaunjones65@gmail.com>
2026-08-31 20:08:43 +0100

    display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("you clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
-function runGame() {
-    let = num1 = Math.floor(Math.random() * 25) + 1;
-    let = num2 = Math.floor(Math.random() * 25) + 1;
+function runGame(gametype) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gametype === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gametype}`);
+        throw `Unknown game type: ${gametype}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -33,8 +40,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
